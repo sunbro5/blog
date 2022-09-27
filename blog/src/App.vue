7547b9e31d51d4b12d0807249ce1f98c8ed9afdb
Jan <maresjan694@gmail.com>
2022-09-27 23:22:42 +0200

    first commit

---
+++ blog/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import PictureRow from './components/PictureRow.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <PictureRow msg="You did it!" />
+
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ blog/src/components/PictureRow.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="picture">
+    <image src="{{name}}.jpg" height="300" width="200"/>
+  </div>
+</template>
+
+<style scoped>
+
+
+</style>
